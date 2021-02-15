6115f629d22a17dc327b020b424620eab9e0bbd5
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-02-15 16:22:55 -0500

    commit tree 03ae8472f1734b7bda24c1a0fb18a526bb516326

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here!
+
+let main = document.querySelector("#main");
+
+main.remove();
+
+let newHeader = document.createElement('h1');
+
+document.body.appendChild(newHeader);
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "Lindsay is the champion"
+
+
+
+
+
+
+
+
+
+
+
